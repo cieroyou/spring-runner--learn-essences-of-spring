dc10ef2f33d20154c73790146eed240e1ff4fd1d
SeraLee <cieroyou1@gmail.com>
2021-10-02 19:35:39 +0900

    [section_04] MovieFinderTest 를 Junit 기반으로 코드 변경

--- src/test/java/moviebuddy/domain/MovieFinderTest.java
+++ src/test/java/moviebuddy/domain/MovieFinderTest.java
@@ -4,26 +4,28 @@ import moviebuddy.MovieBuddyApplication;
 import moviebuddy.MovieBuddyFactory;
 import moviebuddy.domain.Movie;
 import moviebuddy.domain.MovieFinder;
+import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovieFinderTest {
-    public static void main(String[] args) {
-        MovieBuddyApplication application = new MovieBuddyApplication();
-        MovieFinder movieFinder = new MovieBuddyFactory().movieFinder();
+    // 테스트 대상을 함수에서 외부로 이동
+    MovieBuddyApplication application = new MovieBuddyApplication();
+    MovieFinder movieFinder = new MovieBuddyFactory().movieFinder();
+
+    @Test
+    void NotEmpty_directedBy() {
         List<Movie> result = movieFinder.directedBy("Michael Bay");
         assertEquals(3, result.size());
+    }
 
-        result = movieFinder.releasedYearBy(2015);
+    @Test
+    void NotEmpty_ReleasedYearBy() {
+        List<Movie> result = movieFinder.releasedYearBy(2015);
         assertEquals(225, result.size());
     }
 
-    static void assertEquals(long expected, long actual) {
-        if (expected != actual) {
-            throw new RuntimeException(String.format("actual(%d) is different from the expected(%d)", actual, expected));
-        }
-    }
 
 }
