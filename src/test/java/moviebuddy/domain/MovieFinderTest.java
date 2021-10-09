75c254d09efead8a023a6682f34c50124abe7b06
SeraLee <cieroyou1@gmail.com>
2021-10-09 19:14:24 +0900

    [section_05] 의존성 추가, Configuration, Bean 어노테이션을 이용해 MovieBuddyFactory를 빈 구성 정보로 만들고, ApplicationContext가 MovieFinder, MovieReader 객체를 생성하고 관리하도록 코드 변경

--- src/test/java/moviebuddy/domain/MovieFinderTest.java
+++ src/test/java/moviebuddy/domain/MovieFinderTest.java
@@ -5,15 +5,18 @@ import moviebuddy.MovieBuddyFactory;
 import moviebuddy.domain.Movie;
 import moviebuddy.domain.MovieFinder;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MovieFinderTest {
+
+    final ApplicationContext applicationContext = new AnnotationConfigApplicationContext(MovieBuddyFactory.class);
     // 테스트 대상을 함수에서 외부로 이동
-    MovieBuddyApplication application = new MovieBuddyApplication();
-    MovieFinder movieFinder = new MovieBuddyFactory().movieFinder();
+    MovieFinder movieFinder = applicationContext.getBean(MovieFinder.class);
 
     @Test
     void NotEmpty_directedBy() {
