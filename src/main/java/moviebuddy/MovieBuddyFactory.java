343107bcb6db21e819a318fecca80959e978e1bd
SeraLee <cieroyou1@gmail.com>
2021-10-16 18:15:58 +0900

    [section_06] 스프링 구성정보(@Configuration) 두개 추가(DomainModuleConfig,DataSourceModuleConfig), DataSourceModuleConfig 에  MovieReader 빈 등록, DomainModuleConfig에 MovieFinder 빈 등록
    *why: 빈 구성정보를 용도에 따라서 여러 개의 빈 구성 정보를 작성하고,Import 어노테이션으로 한번에 불러 사용할 수 있다

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -6,21 +6,29 @@ import moviebuddy.domain.MovieReader;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Scope;
 
 /**
  * 객체를 생성하고 관리하는 역할
  */
 @Configuration
+@Import({MovieBuddyFactory.DataSourceModuleConfig.class, MovieBuddyFactory.DomainModuleConfig.class})
 public class MovieBuddyFactory {
 
-    @Bean
-    public MovieReader movieReader() {
-        return new CsvMovieReader();
+    @Configuration
+    static class DomainModuleConfig {
+        @Bean
+        public MovieFinder movieFinder(MovieReader movieReader) {
+            return new MovieFinder(movieReader);
+        }
     }
 
-    @Bean
-    public MovieFinder movieFinder(MovieReader movieReader) {
-        return new MovieFinder(movieReader);
+    @Configuration
+    static class DataSourceModuleConfig {
+        @Bean
+        public MovieReader movieReader() {
+            return new CsvMovieReader();
+        }
     }
 }
