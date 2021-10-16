3bcce86eb05c9f5d2304db145fc9ea0e3c227ad1
SeraLee <cieroyou1@gmail.com>
2021-10-16 17:56:26 +0900

    [section_06] 자바코드로 의존관계 주입, 싱글톤 빈에 대한 테스트 코드 작성

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -2,8 +2,11 @@ package moviebuddy;
 
 import moviebuddy.domain.CsvMovieReader;
 import moviebuddy.domain.MovieFinder;
+import moviebuddy.domain.MovieReader;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 /**
  * 객체를 생성하고 관리하는 역할
@@ -12,7 +15,12 @@ import org.springframework.context.annotation.Configuration;
 public class MovieBuddyFactory {
 
     @Bean
-    public MovieFinder movieFinder() {
-        return new MovieFinder(new CsvMovieReader());
+    public MovieReader movieReader() {
+        return new CsvMovieReader();
+    }
+
+    @Bean
+    public MovieFinder movieFinder(MovieReader movieReader) {
+        return new MovieFinder(movieReader);
     }
 }
