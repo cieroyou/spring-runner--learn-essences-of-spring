1327f9dd1cb95ceb4f756492a1fdac89a25c26e8
SeraLee <cieroyou1@gmail.com>
2021-12-06 17:40:42 +0900

    [section_13] 어노테이션을 이용한 프로퍼티 설정

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -18,6 +18,7 @@ import java.util.Properties;
  * 객체를 생성하고 관리하는 역할
  */
 @Configuration
+@PropertySource("/application.properties")
 @ComponentScan
 public class MovieBuddyFactory {
 
@@ -31,27 +32,21 @@ public class MovieBuddyFactory {
 
     @Configuration
     static class DataSourceModuleConfig {
-        private final Environment environment;
 
-        @Autowired
-        public DataSourceModuleConfig(Environment environment){
-            this.environment = environment;
-        }
-
-        @Profile(MovieBuddyProfile.CSV_MODE)
-        @Bean
-        public AbstractFileSystemMovieReader csvMovieReader() {
-            AbstractFileSystemMovieReader movieReader = new CsvMovieReader();
-            movieReader.setMetadata(environment.getProperty("movie.metadata"));
-            return movieReader;
-        }
-
-        @Profile(MovieBuddyProfile.XML_MODE)
-        @Bean
-        public AbstractFileSystemMovieReader xmlMovieReader(Unmarshaller unmarshaller) {
-            AbstractFileSystemMovieReader movieReader = new XmlMovieReader(unmarshaller);
-            movieReader.setMetadata(environment.getProperty("movie.metadata"));
-            return movieReader;
-        }
+//        @Profile(MovieBuddyProfile.CSV_MODE)
+//        @Bean
+//        public AbstractFileSystemMovieReader csvMovieReader() {
+//            AbstractFileSystemMovieReader movieReader = new CsvMovieReader();
+////            movieReader.setMetadata(environment.getProperty("movie.metadata"));
+//            return movieReader;
+//        }
+//
+//        @Profile(MovieBuddyProfile.XML_MODE)
+//        @Bean
+//        public AbstractFileSystemMovieReader xmlMovieReader(Unmarshaller unmarshaller) {
+//            AbstractFileSystemMovieReader movieReader = new XmlMovieReader(unmarshaller);
+////            movieReader.setMetadata(environment.getProperty("movie.metadata"));
+//            return movieReader;
+//        }
     }
 }
