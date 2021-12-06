31c1671c7a18800f1c2e18cc4d786777abb5ff46
SeraLee <cieroyou1@gmail.com>
2021-12-06 19:53:52 +0900

    [section_14] xmlMovieReader에 classLoader 에서 스프링 Resource interface를 이용하여 메타데이터를 읽어들이도록 변경

--- src/main/java/moviebuddy/data/XmlMovieReader.java
+++ src/main/java/moviebuddy/data/XmlMovieReader.java
@@ -34,7 +34,7 @@ public class XmlMovieReader extends AbstractMetadataResourceMovieReader implemen
     @Override
     public List<Movie> loadMovies() {
         try {
-            final InputStream content = ClassLoader.getSystemResourceAsStream(getMetadata());
+            final InputStream content = getMetadataResource().getInputStream();
             final Source source = new StreamSource(content);
             final MovieMetadata metadata = (MovieMetadata) unmarshaller.unmarshal(source);
 
